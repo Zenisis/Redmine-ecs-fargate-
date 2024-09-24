439c505f77fe7adc63dd8a0a6286bf9f5b5fea21
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-09-24 16:08:03 +0530

    Update vpc.tf

--- ecs-fargate/vpc.tf
+++ ecs-fargate/vpc.tf
@@ -5,3 +5,22 @@ resource "aws_vpc" "main" {
     Name = "redmine-vpc"
   }
 }
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"  # Change this to match your region
+  
+  tags = {
+    Name = "redmine-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"  # Change this to match your region
+  
+  tags = {
+    Name = "redmine-public-subnet-2"
+  }
+}
