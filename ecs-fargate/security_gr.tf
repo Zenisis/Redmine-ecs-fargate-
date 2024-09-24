48733ad99efa2a6f13acda439932f976ad70a82a
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-09-24 16:10:02 +0530

    Create security_gr.tf

---
+++ ecs-fargate/security_gr.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "redmine" {
+  name        = "redmine-sg"
+  description = "Allow inbound traffic for Redmine"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "redmine-sg"
+  }
+}
