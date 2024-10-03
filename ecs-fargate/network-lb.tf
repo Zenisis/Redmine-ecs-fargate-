3b3b260671632da0c47685c8208d42d94a0c282f
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-10-03 15:47:02 +0530

    Update network-lb.tf

--- ecs-fargate/network-lb.tf
+++ ecs-fargate/network-lb.tf
@@ -10,3 +10,29 @@ resource "aws_lb" "network" {
     Name = "network-alb"
   }
 }
+# Create NLB target group
+resource "aws_lb_target_group" "redmine_nlb_tg" {
+  name        = "redmine-nlb-tg"
+  port        = 80
+  protocol    = "TCP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "alb"
+
+  health_check {
+    protocol = "HTTP"
+    path     = "/"
+    port     = "traffic-port"
+  }
+}
+
+# Create NLB listener
+resource "aws_lb_listener" "redmine_nlb_listener" {
+  load_balancer_arn = aws_lb.redmine_nlb.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.redmine_nlb_tg.arn
+  }
+}
