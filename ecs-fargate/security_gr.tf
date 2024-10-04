fe7a35ad4c50e94da87b0ddfe1c8dd0e47b7186e
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-10-04 11:38:12 +0530

    Update security_gr.tf

--- ecs-fargate/security_gr.tf
+++ ecs-fargate/security_gr.tf
@@ -2,16 +2,15 @@ resource "aws_security_group" "redmine" {
   name        = "redmine-sg"
   description = "Allow inbound traffic for Redmine"
   vpc_id      = aws_vpc.main.id
-
-   dynamic "ingress" {
+     dynamic "ingress" {
     for_each = [0, 22, 443, 80, 8080, 1337, 8000, 8001, 8005, 8004,3000 -1]
     content {
       from_port   = 0
       to_port     = 0
       protocol    = "-1" # Use "-1" for all protocols
       cidr_blocks = ["0.0.0.0/0"]
+    }
   }
-  
 
   egress {
     from_port   = 0
@@ -19,7 +18,7 @@ resource "aws_security_group" "redmine" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   tags = {
     Name = "redmine-sg"
   }
